1997186445c03d98885fe69158607a648178a95b
Tushar_KG <tusharkg2003@gmail.com>
2025-07-28 22:07:31 +0530

    Added frequency counting and sum feature in array.c

--- arrays/1D_array.c
+++ arrays/1D_array.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-// #include <conio.h>
+#include <conio.h>
 
 int main(){
-    int Arr[25],i,n,p;
+    int Arr[25],i;
 
     // TAKING INPUT FROM THE USER AND STORING IT
-
+    int n;
     printf("How many no you want to store: ");
     scanf("%d",&n);
 
@@ -22,7 +22,7 @@ int main(){
     }
 
     // TAKING INPUT AND CHECKING IF ITS IN THE ARRAY
-
+    int p;
     printf("\nNumber you want to search: ");
     scanf("%d",&p);
 
@@ -36,6 +36,24 @@ int main(){
         if(i==n){
             printf("not found");
         }
+
+    // TAKING INPUT FROM THE USER TO COUNT THE FREQUENCY OF
+    int num,count=0;
+    printf("\nNumber you want to count: ");
+    scanf("%d",&num);
+    for(i = 0;i < n; i++){
+        if(Arr[i]==num){
+            count++;
+        }
+    }
+        printf("Frequency is: %d ",count);
     
+
+    // SUM OF ARRAY
+    int t=0;
+    for(i = 0;i < n; i++){
+        t=t+Arr[i];
+    }
+    printf("Sum= %d ",t);
     return 0;
 }
