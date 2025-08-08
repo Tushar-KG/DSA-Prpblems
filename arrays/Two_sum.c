4d3bc2b7ac767d6d3865287413e58f96a59c76f2
Tushar_KG <tusharkg2003@gmail.com>
2025-08-08 15:05:53 +0530

    two sum  from leetcode

---
+++ arrays/Two_sum.c
@@ -0,0 +1,39 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize=2;
+    int* result=(int*)malloc(2 * sizeof(int));
+    for (int i=0;i<numsSize;i++) {
+        for (int j=i+1; j<numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+
+    *returnSize=0;
+    return NULL;
+}
+int main() {
+    int nums[] = {2, 7, 11, 15};
+    int target = 9;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int returnSize;
+    
+    int* result = twoSum(nums, numsSize, target, &returnSize);
+
+    if (result != NULL) {
+        printf("Indices: [%d, %d]\n", result[0], result[1]);
+        free(result); // Don't forget to free allocated memory
+    } else {
+        printf("No two elements sum up to the target.\n");
+    }
+
+    return 0;
+}
